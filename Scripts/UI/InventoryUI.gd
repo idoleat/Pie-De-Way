f3f51a018423b367f96de0ef724642ac1c7005c2
PJ_Yu <pinjayyu@gmail.com>
2021-09-04 23:47:15 +0800

    Update inventory rules

--- Scripts/UI/InventoryUI.gd
+++ Scripts/UI/InventoryUI.gd
@@ -3,7 +3,7 @@ class_name InventoryUI
 
 var panel_count = 7
 var panel_list = []
-var item_name_list = []
+var item_id_dict = {}
 
 func _ready():
 	get_all_panel_references()
@@ -13,8 +13,17 @@ func get_all_panel_references():
 		panel_list.append(get_node("ItemList/Panel" + str(i)))
 
 func show_item(item_name : String):
-	if item_name_list.has(item_name):
+	if item_id_dict.has(item_name):
 		return
 	
 	var data = DataManager.get_item_data(item_name)
-	panel_list[0].get_stylebox("panel").set_texture(data.icon)
+	if data == null:
+		print("Can't get correct item data:" + item_name)
+		return
+	
+	if !item_id_dict.has(item_name):
+		for i in panel_count:
+			if not i in item_id_dict.values():
+				panel_list[i].get_stylebox("panel").set_texture(data.icon)
+				item_id_dict[item_name] = i
+				break
