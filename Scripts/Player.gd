7002ce2e78a703d9ee1ce0fdd15f78d9f6763803
C.W. Chien <dppss92132@gmail.com>
2021-09-04 16:16:09 +0800

    Set up Player vs NPC behavior when collided

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -3,6 +3,7 @@ extends KinematicBody2D
 export (int) var speed = 200
 
 var target_position = position
+var last_collider_id
 
 func _ready():
 	EventManager.listen(GameEvents.mouse_pressed, funcref(self, "get_input"))
@@ -13,6 +14,16 @@ func get_input(input_position : Vector2):
 func _physics_process(delta):
 	var new_velocity = calculate_new_velocity(delta)
 	move_and_slide(new_velocity)
+	if (get_slide_count()):
+		for i in get_slide_count(): 
+			var collision = get_slide_collision(i)
+			if(last_collider_id == collision.collider_id): continue
+			if (collision.collider.has_method("Collided")):
+				collision.collider.Collided()
+			print("I collided with ", collision.collider.name)
+			last_collider_id = collision.collider_id
+	else:
+		last_collider_id = 0
 
 func calculate_new_velocity(delta) -> Vector2:
 	var delta_position : Vector2 = target_position - position
