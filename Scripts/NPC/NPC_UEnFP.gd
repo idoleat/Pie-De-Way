6d914b077bc922a942306c1780e38ff23eb952c1
C.W. Chien <dppss92132@gmail.com>
2021-09-04 23:42:55 +0800

    Make UberEast and FoodPanda use the same script
    Also won't triggered on input. Also make them collide on layer2,
    avoiding other NPCs. Or player will get item when UE/FP run into cat

--- Scripts/NPC/NPC_UEnFP.gd
+++ Scripts/NPC/NPC_UEnFP.gd
@@ -1,14 +1,23 @@
 extends KinematicBody2D
 
 export (int) var speed = 100
+export (bool) var onA
 
-var target_position = position
+var A_position
+var B_position
+var target_position
 var last_collider_id
+
 
 func _ready():
 	EventManager.listen(GameEvents.mouse_pressed, funcref(self, "get_input"))
+	A_position = get_node("../A").position
+	B_position = get_node("../B").position
+	if (onA): position = A_position
+	else: position = B_position
+	target_position = position
 
-func get_input(input_position : Vector2):
+func Deliver(input_position : Vector2):
 	target_position = get_node("../Player").position
 
 func _physics_process(delta):
@@ -20,8 +29,7 @@ func _physics_process(delta):
 			if(last_collider_id == collision.collider_id): continue
 			if (collision.collider.has_method("Collided")):
 				collision.collider.Collided()
-			print("I collided with ", collision.collider.name)
-			target_position = get_node("../B").position
+				swap_position()
 			last_collider_id = collision.collider_id
 	else:
 		last_collider_id = 0
@@ -36,3 +44,11 @@ func calculate_new_velocity(delta) -> Vector2:
 		veloctiy = delta_position * speed * delta
 	
 	return veloctiy
+
+func swap_position():
+	if (onA):
+		target_position = B_position
+		onA = false
+	else:
+		target_position = A_position
+		onA = true
